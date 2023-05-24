d527bea90c4251553191eaf5dccedc1bd986d5a8
Sagar Mainali <sagarmainali78@gmail.com>
2023-05-24 18:41:41 +0545

    template ready for logic build

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css'
 
 function App() {
+     const [messages, setMessages] = useState([])
+     function add() {
+          
+     }
      return (
-          <div>
-               <button>Add to cart</button>
-               <span>"Added to cart"</span>
+          <div className='app'>
+               <button onClick={add}>Add to cart</button>
+               {messages.map(message => <span>"Added to cart"</span>)}
           </div>
      )
 }
