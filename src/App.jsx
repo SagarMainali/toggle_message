586d043bc7ae1c4e0bdedcfba268dff045d0f204
Sagar Mainali <sagarmainali78@gmail.com>
2023-05-24 18:37:05 +0545

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './style.css'
+
+function App() {
+     return (
+          <div>
+               <button>Add to cart</button>
+               <span>"Added to cart"</span>
+          </div>
+     )
+}
+
+export default App
