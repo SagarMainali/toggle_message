f0659a6f9127b5c3f8e350ad4e59c8dcc4a197c3
Sagar Mainali <sagarmainali78@gmail.com>
2023-05-25 11:38:12 +0545

    added functionality to show and remove message

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './style.css'
 
 function App() {
      const [messages, setMessages] = useState([])
      function add() {
-          
+          setMessages(prevMessages => ([...prevMessages, "Added to cart"]))
      }
+     useEffect(() => {
+          const timer = setTimeout(() => {
+               setMessages(prevMessages => prevMessages.slice(1))
+          }, 1000)
+
+          return () => {
+               clearTimeout(timer)
+          }
+     }, [messages])
      return (
           <div className='app'>
                <button onClick={add}>Add to cart</button>
-               {messages.map(message => <span>"Added to cart"</span>)}
+               {messages.map((message, index) => <span key={index}>"{index} - {message}"</span>)}
           </div>
      )
 }
