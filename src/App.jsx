cf7cb9a4d901e5ac3bc2043d33cf636ef7436439
Sagar Mainali <sagarmainali78@gmail.com>
2023-05-25 21:38:11 +0545

    added another modification to the code and commented the prev one

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,75 @@
-import React, { useEffect, useState } from 'react'
+// import React, { useState } from 'react'
+// import './style.css'
+
+// function App() {
+
+//      const [messages, setMessages] = useState([])
+
+//      function showMessage(e) {
+//           const { name } = e.target
+//           const string = name === 'add' ? 'Added to cart' : 'Removed from cart'
+//           setMessages(prevMessages => ([...prevMessages, string]))
+//           setTimeout(() => {
+//                // can't use array.shift() to remove the first element because it modifies the state directly
+//                setMessages(prevMessages => prevMessages.slice(1))
+//           }, 2000)
+//      }
+
+//      return (
+//           <div className='app'>
+//                <button onClick={showMessage} name='add'>Add to cart</button>
+//                <button onClick={showMessage} >Remove from cart</button>
+//                <div className="messages">
+//                     {messages.map((message, index) => <span key={index}>"{message}"</span>)}
+//                </div>
+//           </div>
+//      )
+// }
+
+// export default App
+
+import React, { useState, useEffect, useRef } from 'react'
 import './style.css'
 
 function App() {
 
-     const [messages, setMessages] = useState([])
+     const [message, setMessage] = useState('')
+     const messageRef = useRef('');
+
 
      function showMessage(e) {
           const { name } = e.target
           const string = name === 'add' ? 'Added to cart' : 'Removed from cart'
-          setMessages(prevMessages => ([...prevMessages, string]))
-          setTimeout(() => {
-               // can't use array.shift() to remove the first element because it modifies the state directly
-               setMessages(prevMessages => prevMessages.slice(1))
-          }, 2000)
+          setMessage(string)
      }
+
+     function showMessage(e) {
+          const { name } = e.target;
+          const string = name === 'add' ? 'Added to cart' : 'Removed from cart';
+          setMessage(string);
+        }
+      
+        useEffect(() => {
+          messageRef.current = message;
+      
+          if (message.length > 0) {
+            console.log('first');
+            const timer = setTimeout(() => {
+              if (messageRef.current === message) {
+                setMessage('');
+              }
+            }, 1000);
+      
+            return () => clearTimeout(timer);
+          }
+        });
 
      return (
           <div className='app'>
                <button onClick={showMessage} name='add'>Add to cart</button>
                <button onClick={showMessage} >Remove from cart</button>
                <div className="messages">
-                    {messages.map((message, index) => <span key={index}>"{message}"</span>)}
+                    {message.length > 0 && <span>"{message}"</span>}
                </div>
           </div>
      )
