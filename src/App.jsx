0680c94547ec1852004b42c097fe7652bb81ae3f
Sagar Mainali <sagarmainali78@gmail.com>
2023-05-25 18:46:22 +0545

    updated the messages to be displayed on specific order

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import React, { useEffect, useState } from 'react'
 import './style.css'
 
 function App() {
+
      const [messages, setMessages] = useState([])
+
      function add() {
           setMessages(prevMessages => ([...prevMessages, "Added to cart"]))
      }
+
      useEffect(() => {
+          // this condition prevents unnecessary renders
           if (messages.length > 0) {
-               console.log('first')
                const timer = setTimeout(() => {
+                    // can't use array.shift() to remove the first element because it modifies the state directly
                     setMessages(prevMessages => prevMessages.slice(1))
                }, 1000)
                return () => {
@@ -17,10 +21,13 @@ function App() {
                }
           }
      }, [messages])
+
      return (
           <div className='app'>
                <button onClick={add}>Add to cart</button>
-               {messages.map((message, index) => <span key={index}>"{index} - {message}"</span>)}
+               <div className="messages">
+                    {messages.map((message, index) => <span key={index}>"{index} - {message}"</span>)}
+               </div>
           </div>
      )
 }
